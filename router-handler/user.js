3eba3de05053e3312e27a3871a4a84515d7dc438
李达康 <hbxslyg@163.com>
2023-02-27 16:40:53 +0800

    style(user): 跳转 user 模块暴露方式

--- router-handler/user.js
+++ router-handler/user.js
@@ -3,82 +3,73 @@ const config = require("../config");
 
 const ok = {
   code: 0,
-  msg: 'ok'
-}
+  msg: "ok",
+};
 
-module.exports = {
-  /**
-   * 登录 
-   * @param {objectj} req 
-   * @param req.body.username 用户名
-   * @param req.body.password 密码
-   * @param {object} res 
-   * @param res.token
-   * @param res.data 用户信息
-   * @returns 
-   */
-  login(req, res) {
-    const { username, password } = req.body;
+/**
+ * 登录
+ * @param {objectj} req
+ * @param req.body.username 用户名
+ * @param req.body.password 密码
+ * @param {object} res
+ * @param res.token
+ * @param res.data 用户信息
+ * @returns
+ */
+exports.login = (req, res) => {
+  const { username, password } = req.body;
 
-    if (username !== "ldk" || password !== "111")
-      return res.send({
-        code: 1,
-        msg: "账号或密码错误",
-      });
-
-    const token = jwt.sign({ username }, config.tokenKey, { expiresIn: "600s" });
-
-    res.send({
-      ...ok,
-      data: {
-        username,
-      },
-      token,
+  if (username !== "ldk" || password !== "111")
+    return res.send({
+      code: 1,
+      msg: "账号或密码错误",
     });
-  },
 
-  /**
-   * 退出
-   * @param {*} req 
-   * @param {*} res 
-   */
-  logout(req, res) {
-    res.send(ok);
-  },
+  const token = jwt.sign({ username }, config.tokenKey, { expiresIn: "600s" });
 
-  /**
-   * 获取用户信息
-   * @param {*} req 
-   * @param {*} res 
-   */
-  getUserInfo(req, res) {
-    res.send({
-      code: 0,
-      msg: "",
-      data: {
-        ...req.auth,
-        iat: undefined,
-        exp: undefined
-      },
-    });
-  },
+  res.send({
+    ...ok,
+    data: {
+      username,
+    },
+    token,
+  });
+};
 
-  /**
+/**
+ * 退出
+ * @param {*} req
+ * @param {*} res
+ */
+exports.logout = function (req, res) {
+  res.send(ok);
+};
+/**
+ * 获取用户信息
+ * @param {*} req
+ * @param {*} res
+ */
+exports.getUserInfo = function (req, res) {
+  res.send({
+    code: 0,
+    msg: "",
+    data: {
+      ...req.auth,
+      iat: undefined,
+      exp: undefined,
+    },
+  });
+};
+
+/**
    * 注册
    * @param {*} req 
    * @param {*} res 
-   * 
-   * 
-   * {
-   *    username: string
-   *    password: string
-   *    email: string
-   * }
    */
-  register(req, res) {
-    
-    res.send(ok)
-  }
+exports.register = function (req, res) {
+   
+  res.send(ok)
+
 
 
 };
